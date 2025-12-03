4718e42e8a87def2ef2346cd6101188ece42ce29
jane <jayndiritu36@gmail.com>
2025-12-03 21:42:10 +0300

    fixing the space characters in graphql

--- my-app/src/graphql/mutations.ts
+++ my-app/src/graphql/mutations.ts
@@ -1,6 +1,7 @@
-import { gql } from "@apollo/client";
-
-export const CREATE_POLL = gql`
+import { gql } 
+from 
+  "@apollo/client";
+export const CREATE_POLL = gql `
   mutation CreatePoll($question: String!, $options: [String!]!) {
     createPoll(question: $question, options: $options) {
       question
@@ -9,13 +10,12 @@ export const CREATE_POLL = gql`
     }
   }
 `;
-
-export const VOTE = gql`
+export const VOTE = gql `
   mutation Vote($optionIndex: Int!) {
     vote(optionIndex: $optionIndex) {
       question
       options
       votes
-    }
-  }
+}
+}
 `;
